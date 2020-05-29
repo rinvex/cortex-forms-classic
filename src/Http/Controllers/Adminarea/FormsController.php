1455e04ed854b3c1e583dc50d68b834b07bc9f1c
Abdelrahman Omran <me@omranic.com>
2020-05-29 05:05:25 +0400

    Drop using strip_tags on redirect identifiers as they will use ->getRouteKey() which is already safe

--- src/Http/Controllers/Adminarea/FormsController.php
+++ src/Http/Controllers/Adminarea/FormsController.php
@@ -235,7 +235,7 @@ class FormsController extends AuthorizedController
 
         return intend([
             'url' => route('adminarea.forms.index'),
-            'with' => ['success' => trans('cortex/foundation::messages.resource_saved', ['resource' => trans('cortex/forms::common.form'), 'identifier' => strip_tags($form->slug)])],
+            'with' => ['success' => trans('cortex/foundation::messages.resource_saved', ['resource' => trans('cortex/forms::common.form'), 'identifier' => $form->slug])],
         ]);
     }
 
@@ -254,7 +254,7 @@ class FormsController extends AuthorizedController
 
         return intend([
             'url' => route('adminarea.forms.index'),
-            'with' => ['warning' => trans('cortex/foundation::messages.resource_deleted', ['resource' => trans('cortex/forms::common.form'), 'identifier' => strip_tags($form->slug)])],
+            'with' => ['warning' => trans('cortex/foundation::messages.resource_deleted', ['resource' => trans('cortex/forms::common.form'), 'identifier' => $form->slug])],
         ]);
     }
 }
