757a51e2d06fcb67d5a7a3c2462b2fef3c95906f
Abdelrahman Omran <me@omranic.com>
2021-05-25 17:57:08 +0400

    Replace deprecated `Breadcrumbs::register` with `Breadcrumbs::for`

--- routes/breadcrumbs/adminarea.php
+++ routes/breadcrumbs/adminarea.php
@@ -6,39 +6,39 @@ use Cortex\Forms\Models\Form;
 use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
     $breadcrumbs->push(trans('cortex/forms::common.forms'), route('adminarea.cortex.forms.forms.index'));
 });
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.import', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.import', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.forms.forms.index');
     $breadcrumbs->push(trans('cortex/forms::common.import'), route('adminarea.cortex.forms.forms.import'));
 });
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.import.logs', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.import.logs', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.forms.forms.index');
     $breadcrumbs->push(trans('cortex/forms::common.import'), route('adminarea.cortex.forms.forms.import'));
     $breadcrumbs->push(trans('cortex/forms::common.logs'), route('adminarea.cortex.forms.forms.import.logs'));
 });
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.create', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.create', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.forms.forms.index');
     $breadcrumbs->push(trans('cortex/forms::common.create_form'), route('adminarea.cortex.forms.forms.create'));
 });
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.edit', function (Generator $breadcrumbs, Form $form) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.edit', function (Generator $breadcrumbs, Form $form) {
     $breadcrumbs->parent('adminarea.cortex.forms.forms.index');
     $breadcrumbs->push(strip_tags($form->name), route('adminarea.cortex.forms.forms.edit', ['form' => $form]));
 });
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.logs', function (Generator $breadcrumbs, Form $form) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.logs', function (Generator $breadcrumbs, Form $form) {
     $breadcrumbs->parent('adminarea.cortex.forms.forms.index');
     $breadcrumbs->push(strip_tags($form->name), route('adminarea.cortex.forms.forms.edit', ['form' => $form]));
     $breadcrumbs->push(trans('cortex/forms::common.logs'), route('adminarea.cortex.forms.forms.logs', ['form' => $form]));
 });
 
-Breadcrumbs::register('adminarea.cortex.forms.forms.responses', function (Generator $breadcrumbs, Form $form) {
+Breadcrumbs::for('adminarea.cortex.forms.forms.responses', function (Generator $breadcrumbs, Form $form) {
     $breadcrumbs->parent('adminarea.cortex.forms.forms.index');
     $breadcrumbs->push(strip_tags($form->name), route('adminarea.cortex.forms.forms.edit', ['form' => $form]));
     $breadcrumbs->push(trans('cortex/forms::common.responses'), route('adminarea.cortex.forms.forms.responses', ['form' => $form]));
--- routes/breadcrumbs/frontarea.php
+++ routes/breadcrumbs/frontarea.php
@@ -6,17 +6,17 @@ use Cortex\Forms\Models\Form;
 use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('frontarea.cortex.forms.forms.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('frontarea.cortex.forms.forms.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('frontarea.home'));
     $breadcrumbs->push(trans('cortex/forms::common.forms'), route('frontarea.cortex.forms.forms.index'));
 });
 
-Breadcrumbs::register('frontarea.cortex.forms.forms.show', function (Generator $breadcrumbs, Form $form) {
+Breadcrumbs::for('frontarea.cortex.forms.forms.show', function (Generator $breadcrumbs, Form $form) {
     $breadcrumbs->parent('frontarea.cortex.forms.forms.index');
     $breadcrumbs->push(strip_tags($form->name), route('frontarea.cortex.forms.forms.show', ['form' => $form]));
 });
 
-Breadcrumbs::register('frontarea.cortex.forms.forms.embed', function (Generator $breadcrumbs, Form $form) {
+Breadcrumbs::for('frontarea.cortex.forms.forms.embed', function (Generator $breadcrumbs, Form $form) {
     $breadcrumbs->parent('frontarea.cortex.forms.forms.index');
     $breadcrumbs->push(strip_tags($form->name), route('frontarea.cortex.forms.forms.embed', ['form' => $form]));
 });
